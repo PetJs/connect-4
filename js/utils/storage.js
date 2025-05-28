1354efd52e945566d3630c835634073db680edbc
PetJs <peterfagoroye76@gmail.com>
2025-05-28 23:06:17 +0100

    feat: :sparkles: Adds Storage for wins and level

---
+++ js/utils/storage.js
@@ -0,0 +1,13 @@
+
+
+export function getUserData(username) {
+    const data = JSON.parse(localStorage.getItem("connect4_users")) || {};
+    return data[username] || { wins: 0, losses: 0, level: 1 };
+}
+
+export function saveUserData(username, userData) {
+    const data = JSON.parse(localStorage.getItem("connect4_users")) || {};
+    data[username] = userData;
+    localStorage.setItem("connect4_users", JSON.stringify(data));
+}
+
