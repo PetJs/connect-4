2928f6b1de0018d4c86cda18d1cb16c1d2185296
PetJs <peterfagoroye76@gmail.com>
2025-05-30 17:52:18 +0100

    feat: :sparkles: Adds Losing sound

--- js/utils/audio.js
+++ js/utils/audio.js
@@ -1,6 +1,7 @@
 const entersfx = new Audio('/assets/audio/the-notification-email-143029.mp3');
 const clicksfs = new Audio('assets/audio/click-151673.mp3');
 const winsfs = new Audio("assets/audio/brass-fanfare-with-timpani-and-winchimes-reverberated-146260.mp3")
+const losesfx = new Audio("assets/audio/mixkit-player-losing-or-failing-2042.wav")
 
 export function enterSound(){
     entersfx.volume = 0.75;
@@ -15,4 +16,9 @@ export function clickSound(){
 export function winSound(){
     winsfs.volume = 0.75;
     winsfs.play()
+}
+
+export function loseSound(){
+    losesfx.volume = 0.75;
+    losesfx.play()
 }
